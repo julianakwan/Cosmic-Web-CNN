6e340a9eeb219f2c0342bafcce57c4ea03bd568b
Juliana Kwan <j.kwan@ljmu.ac.uk>
2023-07-06 22:51:39 +0100

    improved image projection

--- all_images_classify_w.py
+++ all_images_classify_w.py
@@ -20,56 +20,58 @@ def load_datacubes(dens_dir):
     
     return dens3d_w0p5, dens3d_w1p0, dens3d_w1p5, dens3d_w2p0
 
-def flatten_dens_to_images(nsub, Npix, dens3d):  
-#convert 3D density cube into 2D projected images
-    
-    images = np.zeros([nsub**3,Npix,Npix])
-    
-    #Collapse 3d voxels into 2d projection for all cubes
-    for i in range (0,nsub):
-        for j in range(0,nsub):
-            for k in range(0,nsub):
-                index = i*nsub**2+j*nsub+k
 
-                dens2d = np.sum(dens3d[i*Npix:(i+1)*Npix,j*Npix:(j+1)*Npix,k*Npix:(k+1)*Npix],axis=2)
-                images[index,:,:] = np.log10(dens2d+1)
-
-
-    return images
-
-
-def flatten_dens_to_images_test(nx, ny, nz, dens3d):  
+def flatten_dens_to_images(nx, ny, nz, dens3d):  
 #convert 3D density cube into 2D projected images
 
 
-    Npix = nx*ny
+#    nx  = length along x in Mpc/h
+#    ny  = length along y in Mpc/h
+#    nz  = length along z in Mpc/h
+#    dens3d = original 3D density cube
+
+    BoxSize = 400  # size of the simulation 400 Mpc/h - this is set
+    TotPix = 1600  # CIC deposition is 1600 x 1600 x 1600 - this is also set
+
+    npix = 400/1600 #each pixel is npix wide i.e. 0.25 Mpc/h
+
+    NpixX = int(nx/npix) # number of pixels in x direction
+    NpixY = int(ny/npix) # number of pixels in y direction
+    NpixZ = int(nz/npix) # number of pixels in z direction
     
-    images = np.zeros([nx*ny*nz,Npix,Npix])
+
+    image_size = [NpixX, NpixY]
+
+    tot_images = int((BoxSize/nx)*(BoxSize/ny)*(BoxSize/nz))
+    
+    images = np.zeros([tot_images,NpixX,NpixY])
+
+    
     
     #Collapse 3d voxels into 2d projection for all cubes
-    for i in range (0,nx):
-        for j in range(0,ny):
-            for k in range(0,nz):
-                index = i*nz**2+j*nz+k
+    for i in range (0,int(BoxSize/nx)):
+        for j in range(0,int(BoxSize/ny)):
+            for k in range(0,int(BoxSize/nz)):
+                index = i*int(BoxSize/nx)**2+j*int(BoxSize/ny)+k
 
-                dens2d = np.sum(dens3d[i*Npix:(i+1)*Npix,j*Npix:(j+1)*Npix,k*Npix:(k+1)*Npix],axis=2)
+                dens2d = np.sum(dens3d[i*NpixX:(i+1)*NpixX,j*NpixY:(j+1)*NpixY,k*NpixZ:(k+1)*NpixZ],axis=2)
                 images[index,:,:] = np.log10(dens2d+1)
 
 
-    return images
+    return image_size, images
 
-def build_regression_model(Npix):
+def build_regression_model(NpixX,NpixY):
     model = keras.Sequential([
         layers.BatchNormalization(),
-        layers.Conv2D(32, kernel_size=9, activation='relu', input_shape=(Npix,Npix,1)),
+        layers.Conv2D(32, kernel_size=9, activation='relu', input_shape=(NpixX,NpixY,1)),
         layers.MaxPool2D(pool_size=(2,2)),
         
         layers.BatchNormalization(),
-        layers.Conv2D(64, kernel_size=5, activation='relu', input_shape=(Npix,Npix,1)),
+        layers.Conv2D(64, kernel_size=5, activation='relu', input_shape=(NpixX,NpixY,1)),
         layers.MaxPool2D(pool_size=(2,2)),
 
         layers.BatchNormalization(),
-        layers.Conv2D(128, kernel_size=3, activation='relu', input_shape=(Npix,Npix,1)),
+        layers.Conv2D(128, kernel_size=3, activation='relu', input_shape=(NpixX,NpixY,1)),
         layers.MaxPool2D(pool_size=(2,2)),
         
 
@@ -87,9 +89,9 @@ def build_regression_model(Npix):
 
     return model
 
-def build_classification_model(Npix):
+def build_classification_model(NpixX,NpixY):
 
-    model = keras.Sequential([keras.layers.Flatten(input_shape=(Npix,Npix,1)),keras.layers.Dense(128,activation=tf.nn.relu),keras.layers.Dense(4,activation=tf.nn.softmax)])
+    model = keras.Sequential([keras.layers.Flatten(input_shape=(NpixX,NpixY,1)),keras.layers.Dense(128,activation=tf.nn.relu),keras.layers.Dense(4,activation=tf.nn.softmax)])
     
     model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
@@ -101,11 +103,10 @@ if __name__ == "__main__":
 
     #These are fixed when the CIC 3d cube was made
 
-#    200 x 200 x 50
 
-    NpixX = 200
-    NpixY = 200
-    NpixZ = 200
+    nx = 50   #x dim of image (in Mpc/h)
+    ny = 50   #y dim of image (in Mpc/h)
+    nz = 10   #z dim of image (in Mpc/h)
     
     Npix = int(100)        #How many pixels
     nsub = int(16)         #The simulation cube is divided into nsub regions
@@ -118,10 +119,12 @@ if __name__ == "__main__":
     dens3d_w0p5, dens3d_w1p0, dens3d_w1p5, dens3d_w2p0 = load_datacubes(dens_dir)
     
     #Get all training images
-    train_images_w0p5 = flatten_dens_to_images(nsub, Npix, dens3d_w0p5)
-    train_images_w1p0 = flatten_dens_to_images(nsub, Npix, dens3d_w1p0)
-    train_images_w1p5 = flatten_dens_to_images(nsub, Npix, dens3d_w1p5)
-    train_images_w2p0 = flatten_dens_to_images(nsub, Npix, dens3d_w2p0)
+    image_size, train_images_w0p5 = flatten_dens_to_images(nx, ny, nz, dens3d_w0p5)
+    image_size, train_images_w1p0 = flatten_dens_to_images(nx, ny, nz, dens3d_w1p0)
+    image_size, train_images_w1p5 = flatten_dens_to_images(nx, ny, nz, dens3d_w1p5)
+    image_size, train_images_w2p0 = flatten_dens_to_images(nx, ny, nz, dens3d_w2p0)
+
+    NpixX, NpixY = image_size
 
     #Combine the images into a single array
     all_images = np.concatenate([train_images_w0p5, train_images_w1p0, train_images_w1p5, train_images_w2p0],axis=0)
@@ -143,9 +146,11 @@ if __name__ == "__main__":
 
                 
     #Split the sample into training and testing models 
- 
-    ntrain = int(16000) #no. of training images
-    training_images = np.zeros([ntrain, Npix, Npix])
+
+    nsplit = 0.8   #percentage of images to use for training
+    
+    ntrain = int(len(all_images)*nsplit) #no. of training images
+    training_images = np.zeros([ntrain, NpixX, NpixY])
     train_indices = np.random.permutation(np.arange(len(all_images)))  #make random permutation of image indices
     test_indices = train_indices[ntrain:]    #remove some for testing
     train_indices = train_indices[:ntrain]   #keep the rest for training
@@ -161,14 +166,14 @@ if __name__ == "__main__":
     #Tensorflow requires 4D array: (nimages, nx_pix, ny_pix, channel)
     #channel = 1 if b/w, channel = 3 if rgb
     
-    training_images = training_images.reshape(-1,Npix,Npix,1)
-    test_images = test_images.reshape(-1,Npix,Npix,1)
+    training_images = training_images.reshape(-1,NpixX,NpixY,1)
+    test_images = test_images.reshape(-1,NpixX,NpixY,1)
     
     normalizer = layers.Normalization(input_shape=[1,], axis=None)
     normalizer.adapt(training_images)
     early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=25)
     
-    model = build_regression_model(Npix)
+    model = build_regression_model(NpixX,NpixY)
 
     #the fitting is done with respect to the training sample
     model.fit(training_images, training_labels, epochs = 100, verbose=1, validation_split=0.2, callbacks=[early_stop])
@@ -179,7 +184,7 @@ if __name__ == "__main__":
     
     #######   CNN classification model  #######
     #
-    #model = build_classification_model(Npix)
+    #model = build_classification_model(NpixX,NpixY)
     ##Fit on fiducial model
     #model.fit(training_images, labels, epochs=5)
     #
